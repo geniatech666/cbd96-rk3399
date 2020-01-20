004b574ad4f3c8efb1e5b700c056c34cd9329bf2
geniatech666 <fu.qiang@geniatech.com>
2020-01-20 03:28:00 +0000

    first commit

---
+++ u-boot/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,5 @@
+#define U_BOOT_DATE "Jan 02 2020"
+#define U_BOOT_TIME "09:07:32"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "01/02/2020"
+#define U_BOOT_BUILD_DATE 0x20200102
---
+++ u-boot/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2017.09-g1b4026a"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "aarch64-linux-gnu-gcc (Linaro GCC 6.3-2017.05) 6.3.1 20170404"
+#define LD_VERSION_STRING "GNU ld (Linaro_Binutils-2017.05) 2.27.0.20161019"
